b352be8da389100f4f21ec7fc31fa701ab9b5bb2
Meg303 <megrhinehart@gmail.com>
2014-03-06 20:19:56 -0500

    set up repository for new quiky app

---
+++ quiky/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiky::Application.config.secret_key_base = '36873a79e57c18d12c5314208f12ffab2adf6f2588dc0724ae708465ad97d075bee46c57c28ea31fec016590da32152fb2dbebd136bd559dcf9cf8b26ddb7c5d'
---
+++ quiky/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quiky::Application.config.session_store :cookie_store, key: '_quiky_session'
